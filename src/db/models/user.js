35a379f7d21a426c8a94e46d16c0051a0f1cdc33
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2023-09-12 17:12:30 -0400

    Update user.js

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,34 +1,30 @@
 const knex = require('../knex');
 const authUtils = require('../../utils/auth-utils');
 
+// This class constructor is used by the model to make passwordHash
+// a private property after fetching a user's data from the database. 
+// When the static methods return the User instance to the controller,
+// the User instance will not reveal the passwordHash. Those instances 
+// will also have access to the instance methods isValidPassword and update.
 class User {
   #passwordHash = null;
-
-  // This constructor is used ONLY by the model
-  // to provide the controller with instances that
-  // have access to the instance methods isValidPassword
-  // and update.
+  
   constructor({ id, username, password_hash }) {
     this.id = id;
     this.username = username;
     this.#passwordHash = password_hash;
   }
 
-  static async list() {
-    try {
-      const query = 'SELECT * FROM users';
-      const { rows } = await knex.raw(query);
-      return rows.map((user) => new User(user));
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
-
   static async find(id) {
     try {
+      // The query to execute
       const query = 'SELECT * FROM users WHERE id = ?';
-      const { rows: [user] } = await knex.raw(query, [id]);
+      
+      // `knex.raw()` returns an object with a .rows array containing the results
+      // Here, we extract the first value from the .rows array and call it `user`
+      const { rows: [user] } = await knex.raw(query, [id]); 
+
+      // If the `user` exists, return a new `User` instance to hide the passwordHash. If not, return `null`
       return user ? new User(user) : null;
     } catch (err) {
       console.error(err);
@@ -41,6 +37,17 @@ class User {
       const query = 'SELECT * FROM users WHERE username = ?';
       const { rows: [user] } = await knex.raw(query, [username]);
       return user ? new User(user) : null;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  static async list() {
+    try {
+      const query = 'SELECT * FROM users';
+      const result = await knex.raw(query);
+      return result.rows.map((user) => new User(user));
     } catch (err) {
       console.error(err);
       return null;
